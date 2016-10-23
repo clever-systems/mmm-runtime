686ebc72258e404e045a5811dac496aaa40fd4d7
Axel Rutz <axel.rutz@clever-systems.net>
2016-10-23 19:29:33 +0200

    Improved redis option

--- src/Option/redis.php
+++ src/Option/redis.php
@@ -13,10 +13,11 @@ class redis {
   public static function settings() {
     global $conf;
 
-    $conf['redis_client_interface'] = 'PhpRedis';
-    $conf['lock_inc']               = 'sites/all/modules/redis/redis.lock.inc';
-    $conf['path_inc']               = 'sites/all/modules/redis/redis.path.inc';
-    $conf['cache_backends'][]       = 'sites/all/modules/redis/redis.autoload.inc';
-    $conf['cache_default_class']    = 'Redis_Cache';
+    if (!empty($conf['redis_client_host'])) {
+      $conf['lock_inc']               = 'sites/all/modules/redis/redis.lock.inc';
+      $conf['path_inc']               = 'sites/all/modules/redis/redis.path.inc';
+      $conf['cache_backends'][]       = 'sites/all/modules/redis/redis.autoload.inc';
+      $conf['cache_default_class']    = 'Redis_Cache';
+    }
   }
 }
